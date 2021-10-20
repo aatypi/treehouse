17dc71aa2f6ca85280a36ef5f368aba3b04eca93
Artofaj <info@artofakume.com>
2021-10-20 19:47:16 +0200

    Tweaked titles of searchbar

--- src/components/pages/SearchBar.js
+++ src/components/pages/SearchBar.js
@@ -5,22 +5,26 @@ export function SearchBar() {
   return (
     <section className="max-width searchbar">
       <div className="searchbar-inner">
-        <div>Destination</div>
         <div>
-          Check In
-          <FaCalendar />
+          <h3>Destination</h3>
+          Select your next destination
         </div>
         <div>
-          Check Out
-          <FaCalendar />
+          <h3>Check In</h3>
+          Pick a Date <FaCalendar />
         </div>
         <div>
-          Guests
-          <FaUserFriends />
+          <h3>Check Out</h3>
+          Pick a Date <FaCalendar />
+        </div>
+        <div>
+          <h3>Guests</h3>
+          Select number of Guests <FaUserFriends />
         </div>
       </div>
       <div className="search-icon">
         <FaSearch />
+        <h3>Search</h3>
       </div>
     </section>
   );
