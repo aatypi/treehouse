32675f645fe418f1d7186ce3deaa9706b19590e8
Artofaj <info@artofakume.com>
2021-10-20 17:49:04 +0200

    Added more info to cards

--- src/components/pages/CardRoll.js
+++ src/components/pages/CardRoll.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 class Card extends React.Component {
-    render(){
-        return (
-            <div className="card"> Perro</div>
-            )
-        }
+  render() {
+    return (
+      <div className="card">
+        <div className="card-inner">
+          {this.props.name && <h1> {this.props.name}</h1>}
+          {this.props.wifi && <span> Free Wi-Fi</span>}
+          {this.props.wifi && <span> Fire alarm</span>}
+          {this.props.wifi && <span> High security</span>}
+        </div>
+      </div>
+    );
+  }
 }
 
 export function CardRoll() {
   return (
     <section className="card-roll">
-      <Card />
-      <Card />
-      <Card />
+      <Card name="Home 1" wifi fireAlarm highSecurity/>
+      <Card name="Home 2" />
+      <Card name="Home 3" />
     </section>
-  )
+  );
 }
