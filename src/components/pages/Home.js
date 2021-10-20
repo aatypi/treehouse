cc966e21d786a6a8dad9fc966f079563c846f7b5
Artofaj <info@artofakume.com>
2021-10-20 19:02:58 +0200

    Added stripe

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,10 +3,21 @@ import { CardRoll } from "./CardRoll";
 
 export function Home() {
   return (
-    <section className="max-width">
-      <h1>Hey hey </h1>
+    <>
+      <section className="max-width">
+          serach bar
+      </section>
+      <section className="white-stripe">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni fuga
+        quas aliquid veniam quis doloribus blanditiis eligendi voluptate
+        consequuntur natus.
+      </section>
 
-      <CardRoll />
-    </section>
-  )
+      <section className="max-width">
+        <h2>Our most popular choices</h2>
+
+        <CardRoll />
+      </section>
+    </>
+  );
 }
