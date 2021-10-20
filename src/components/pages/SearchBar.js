c4895d8ebf07b17c7a48806dcd92907fc72d8b9e
Artofaj <info@artofakume.com>
2021-10-20 19:41:52 +0200

    Tweaked on searchbar

--- src/components/pages/SearchBar.js
+++ src/components/pages/SearchBar.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { FaCalendar, FaSearch, FaUserFriends } from "react-icons/fa";
 
 export function SearchBar() {
-  return <section className="max-width">serach bar</section>;
+  return (
+    <section className="max-width searchbar">
+      <div className="searchbar-inner">
+        <div>Destination</div>
+        <div>
+          Check In
+          <FaCalendar />
+        </div>
+        <div>
+          Check Out
+          <FaCalendar />
+        </div>
+        <div>
+          Guests
+          <FaUserFriends />
+        </div>
+      </div>
+      <div className="search-icon">
+        <FaSearch />
+      </div>
+    </section>
+  );
 }
