a6d036a2180ff480094dbfdd7af97c1158cb1765
Artofaj <info@artofakume.com>
2021-10-23 19:27:38 +0200

    Implemented a rudimentary nchor link to the icons

--- src/components/pages/Destinations.js
+++ src/components/pages/Destinations.js
@@ -8,6 +8,14 @@ export function Destinations() {
         <h1>Destinations</h1>
       </section>
       <IconBar />
+
+      <section className="destination" id="Paris">
+        <h1>PARIS</h1>
+      </section>
+
+      <section className="destination" id="London">
+        <h1>LONDON</h1>
+      </section>
     </>
   );
 }
