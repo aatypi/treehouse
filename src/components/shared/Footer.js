dbd0ccbd4c4811d0ca950a11a612158b1e31a363
Artofaj <info@artofakume.com>
2021-10-23 19:16:41 +0200

    Added some elements too the footer. Still needs images for logo. styles not 100% implemented

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -3,8 +3,30 @@ import React from "react";
 export function Footer() {
   return (
     <footer>
-      <h1>Hey hey </h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quia accusamus architecto laboriosam fuga voluptas soluta dolorem eveniet voluptate accusantium ab reiciendis voluptatem officia fugit pariatur repellendus totam velit, placeat illo, quibusdam adipisci doloremque cupiditate ullam. Culpa, possimus saepe fugiat reiciendis minus quam nihil earum eveniet beatae, at quod nulla.
+      <div className="footer-wrapper">
+        <div>
+          Phone Support <br />
+          Lorem ipsum dolor sit <br />
+          Amet consectetur adipisicing
+        </div>
+        <div>
+          logo
+        </div>
+        <div className="subscribe">
+          <h3>sign up</h3>
+          <input type="text" placeholder="your e-mail" />
+          <button className="subscribe-button"> subscribe</button>
+
+          <div className="text-box">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto unde
+            maxime numquam neque inventore, fuga accusamus illum quam? Iure,
+            illo!
+          </div>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, non.
+      </p>
     </footer>
   );
 }
