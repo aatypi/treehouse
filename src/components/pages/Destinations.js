8765858fcce2bbfb2fb149f8043ecd31cc30f317
Artofaj <info@artofakume.com>
2021-10-23 19:46:32 +0200

    Implemented 2 destination sections

--- src/components/pages/Destinations.js
+++ src/components/pages/Destinations.js
@@ -1,5 +1,7 @@
 import React from "react";
 import IconBar from "../shared/IconBar";
+import { Card } from "../shared/CardRoll";
+import icon from "../../images/BIGBEN.svg";
 
 export function Destinations() {
   return (
@@ -10,11 +12,69 @@ export function Destinations() {
       <IconBar />
 
       <section className="destination" id="Paris">
-        <h1>PARIS</h1>
+        <div className="destination-info">
+          <h1>PARIS</h1>
+          <div className="icon">
+            <img src={icon} alt="" width={75} height={75} />
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Recusandae, neque pariatur exercitationem quaerat atque nostrum
+            tempore mollitia ratione voluptates laborum.
+          </p>
+        </div>
+        <div className="destination-cards">
+          <Card
+            name="Home 1"
+            wifi={true}
+            fireAlarm={true}
+            highSecurity={true}
+            reviews={10}
+            stars={5}
+            liked
+          />
+          <Card
+            name="Home 1"
+            wifi={true}
+            fireAlarm={true}
+            highSecurity={true}
+            reviews={10}
+            stars={5}
+            liked
+          />
+        </div>
       </section>
 
       <section className="destination" id="London">
-        <h1>LONDON</h1>
+        <div className="destination-info">
+          <h1>London</h1>
+          <div className="icon">
+            <img src={icon} alt="" width={75} height={75} />
+          </div>
+          <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eius distinctio aliquam adipisci autem excepturi, placeat ipsum id maxime repellat, explicabo nam voluptates dignissimos? In reprehenderit pariatur dolore esse numquam.
+          </p>
+        </div>
+        <div className="destination-cards">
+          <Card
+            name="Home 1"
+            wifi={true}
+            fireAlarm={true}
+            highSecurity={true}
+            reviews={10}
+            stars={5}
+            liked
+          />
+          <Card
+            name="Home 1"
+            wifi={true}
+            fireAlarm={true}
+            highSecurity={true}
+            reviews={10}
+            stars={5}
+            liked
+          />
+        </div>
       </section>
     </>
   );
